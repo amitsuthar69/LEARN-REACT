dff3c9a22d8c07d0d4c5a491b70ca9ca0697b0ec
amit <sutharamit707@gmail.com>
2023-12-13 23:27:08 +0530

    new-project

---
+++ Projects/PasswordGenerator/index.jsx
@@ -0,0 +1,91 @@
+import { useState, useCallback, useEffect, useRef } from "react";
+
+function App() {
+  // variables
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  // creating reference for input field
+  const passwordRef = useRef(null);
+
+  // psword generation logic
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  // clipboard copy logic
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+  return (
+    <div>
+      <h1>Password generator</h1>
+      <div>
+        <input
+          type="text"
+          value={password}
+          placeholder="Password"
+          readOnly
+          ref={passwordRef}
+        />{" "}
+        <button onClick={copyPasswordToClipboard}>copy</button>
+      </div>
+      <div>
+        <div>
+          <input
+            type="range"
+            min={8}
+            max={100}
+            value={length}
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
+          />
+          <label>Length: {length}</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+feild;
